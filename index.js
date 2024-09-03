092cc0e33099917a69c8e0060c9ea52d62a36807
bananatchatcha06 <126298413+bananatchatcha06@users.noreply.github.com>
2024-09-03 23:07:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Console } = require("console")
+
+const primeironome = "Jennifer"
+const sobrenome = "Gralik"
+const idade = 18
+const cidadenatal = "Curitiba"
+const altura = 1.70
+
+console.log("O meu primeiro nome é:");
+console.log(primeironome);
+console.log("Meu sobrenome é:");
+console.log(sobrenome);
+console.log("Minha idade é:");
+console.log(idade);
+console.log("Sou natural de:");
+console.log(cidadenatal);
+console.log("Minha altura é:");
+console.log(altura);
